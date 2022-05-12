376bf4baceeba45ccd8face5baae414d4f2c02de
Dio Lemos <pedrodiogenes@outlook.com>
2022-05-12 08:31:12 -0300

    form sections identified diferently. current section updates

--- form.js
+++ form.js
@@ -3,6 +3,7 @@ const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const circles  = document.querySelectorAll('.circle')
 const form = document.querySelectorAll("form")
+const submitBtn = document.getElementById("submit-button")
 let currentActive = 1;
 
 update()
@@ -28,6 +29,11 @@ prev.addEventListener('click', ()=>{
 
 
 function update(){
+    const section1 = document.getElementById('first-section')
+    const section2 = document.getElementById('second-section')
+    const section3 = document.getElementById('third-section')
+    const section4 = document.getElementById('fourth-section')
+
     circles.forEach((circle, idx)=>{
         if(idx < currentActive){
             circle.classList.add('active')
@@ -38,7 +44,8 @@ function update(){
 
     const actives = document.querySelectorAll('.active')
   progress.style.width = (actives.length -1)/ (circles.length - 1) * 100 + "%"
-
+    
+  //updates prev and next buttons 
   if(currentActive === 1){
       prev.disabled = true
   }else if(currentActive === circles.length){
@@ -47,6 +54,39 @@ function update(){
       prev.disabled = false;
       next.disabled = false;
   }
+
+//   sets the current group of field forms
+  if(currentActive == 1){
+      section1.setAttribute('class', 'current-field')
+      section2.setAttribute('class', 'hidden')
+      section3.setAttribute('class', 'hidden')
+      section4.setAttribute('class', 'hidden')      
+  }else if (currentActive == 2){
+    section1.setAttribute('class', 'hidden')
+    section2.setAttribute('class', 'current-field')
+    section3.setAttribute('class', 'hidden')
+    section4.setAttribute('class', 'hidden') 
+  }else if (currentActive == 3){
+    section1.setAttribute('class', 'hidden')
+    section2.setAttribute('class', 'hidden')
+    section3.setAttribute('class', 'current-field')
+    section4.setAttribute('class', 'hidden') 
+  }else if (currentActive == 4){
+    section1.setAttribute('class', 'hidden')
+    section2.setAttribute('class', 'hidden')
+    section3.setAttribute('class', 'hidden')
+    section4.setAttribute('class', 'current-field') 
+    
+  }
+
+
+  if(currentActive == 4){
+      submitBtn.classList.remove("hidden")
+  }else{
+      submitBtn.classList.add("hidden")
+  }
+
+
 }
 
 document.querySelector("form").addEventListener("click", function(event){
